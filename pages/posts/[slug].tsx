576c7b67b8d802d2bcbe7c3694656bf52baf079f
Sagawa Kazuki <kazuki@A-230.local>
2023-04-06 22:14:32 +0900

    ライブラリを使ってマークダウンを出力

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getAllPosts, getSinglePost } from '@/lib/notionAPI';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 
 export const getStaticPaths = async () => {
     const allPosts = await getAllPosts();
@@ -24,15 +25,23 @@ export const getStaticProps = async ({params}) => {
 function Post({post}) {
   return (
     <section className='container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20'>
-        <h2 className='w-full text-2xl font-medium'>2回目の投稿です。</h2>
+        <h2 className='w-full text-2xl font-medium'>{post.metadata.title}</h2>
         <div className='border-b-2 w-1/3 mt-1 border-sky-900'></div>
-        <span className='text-gray-500'>2023/4/6</span>
+        <span className='text-gray-500'>Posted date at {post.metadata.date}</span>
         <br />
-        <p className='text-white bg-sky-900 rounded-x1 font-medium mt-2 px-2 inline-block'>
-            Next.js
-        </p>
+        {post.metadata.tags.map((tag: string, index: number) => (
+            <p
+                key={index}
+                className='text-white bg-sky-900 rounded-x1 font-medium mt-2 px-2 inline-block'
+            >
+                {tag}
+            </p>
+        ))}
+
         <div className='mt-10 font-medium'>
-            test
+            <ReactMarkdown children={post.markdown} >
+
+            </ReactMarkdown>
         </div>
     </section>
   )
