a40608d258a3a8f6f427b894a0d2375e3f0bfa39
Sagawa Kazuki <kazuki@A-230.local>
2023-04-07 14:29:48 +0900

    もっと見るページのレイアウト修正

--- pages/components/Post/SinglePost.tsx
+++ pages/components/Post/SinglePost.tsx
@@ -6,32 +6,59 @@ type Props = {
     description: string
     data: string
     tags: string[]
-    slug: string
+    slug: string;
+    isPaginationPage: boolean;
 };
 
 export const SinglePost = (props: Props) => {
-    const {title, description, data, tags, slug} = props;
+    const {title, description, data, tags, slug, isPaginationPage} = props;
   return (
-    <section
+    <>
+    {isPaginationPage ? (
+        <section
+        className='bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300'
+        >
+            <div className='lg:flex items-center'>
+                <h2 className='text-gray-100 text-2xl font-medium mb-2'>
+                    <Link href={`/posts/${slug}`}>
+                        {title}
+                    </Link>
+                </h2>
+                <div className='text-gray-200 mr-2'>{data}</div>
+                {tags.map((tag, index) => (
+                    <span
+                        key={index}
+                        className='text-white bg-gray-500 rounded-x1 px-2 pb-1 mr-2 font-medium'
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+            <p className='text-gray-100'>{description}</p>
+        </section>
+    ) : (
+        <section
         className='lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300'
-    >
-        <div className='flex items-center gap-3'>
-            <h2 className='text-gray-100 text-2xl font-medium mb-2'>
-                <Link href={`/posts/${slug}`}>
-                    {title}
-                </Link>
-            </h2>
-            <div className='text-gray-100'>{data}</div>
-            {tags.map((tag, index) => (
-                <span
-                    key={index}
-                    className='text-white bg-gray-500 rounded-x1 px-2 pb-1 font-medium'
-                >
-                    {tag}
-                </span>
-            ))}
-        </div>
-        <p className='text-gray-100'>{description}</p>
-    </section>
+        >
+            <div className='flex items-center gap-3'>
+                <h2 className='text-gray-100 text-2xl font-medium mb-2'>
+                    <Link href={`/posts/${slug}`}>
+                        {title}
+                    </Link>
+                </h2>
+                <div className='text-gray-100'>{data}</div>
+                {tags.map((tag, index) => (
+                    <span
+                        key={index}
+                        className='text-white bg-gray-500 rounded-x1 px-2 pb-1 font-medium'
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+            <p className='text-gray-100'>{description}</p>
+        </section>
+    )}
+    </>
   )
 }
