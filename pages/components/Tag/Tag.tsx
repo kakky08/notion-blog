62307ca539001d21f028c5407b670d7cf353cb5f
Sagawa Kazuki <kazuki@A-230.local>
2023-04-08 11:52:18 +0900

    タグ検索が出来るように実装

--- pages/components/Tag/Tag.tsx
+++ pages/components/Tag/Tag.tsx
@@ -1,32 +1,35 @@
 import Link from 'next/link'
 import React from 'react'
 
-function Tag() {
-  return (
-    <div className='mx-4'>
-        <section
-            className='lg:w-1/2 mb-8 mx-auto bg-orange-200 rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 duration-300 transition-all'
-        >
-            <div className='font-medium mb-4'>タグ検索</div>
-            <div className='flex flex-wrap gap-5'>
-                <Link href="/posts/tag/blog/page/1">
-                    <span
-                        className='cursor-pointer px-2 font-medium pb-1 rounded-x1 bg-gray-400 inline-block'
-                    >
-                        Blog
-                    </span>
-                </Link>
-                <Link href="/posts/tag/blog/page/1">
-                    <span
-                        className='cursor-pointer px-2 font-medium pb-1 rounded-x1 bg-gray-400 inline-block'
-                    >
-                        Test
-                    </span>
-                </Link>
-            </div>
-        </section>
-    </div>
-  )
+type Props = {
+    tags: string[];
+}
+
+function Tag(props: Props) {
+    const { tags } = props;
+    return (
+        <div className='mx-4'>
+            <section
+                className='lg:w-1/2 mb-8 mx-auto bg-orange-200 rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 duration-300 transition-all'
+            >
+                <div className='font-medium mb-4'>タグ検索</div>
+                <div className='flex flex-wrap gap-5'>
+                    {tags.map((tag: string, index: number) => (
+                        <Link
+                            href={`/posts/tag/${tag}/page/1`}
+                            key={index}
+                        >
+                            <span
+                                className='cursor-pointer px-2 font-medium pb-1 rounded-x1 bg-gray-400 inline-block'
+                            >
+                                { tag }
+                            </span>
+                        </Link>
+                    ))}
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Tag
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import { getAllPosts, getPostsTopPage } from '@/lib/notionAPI'
+import { getAllPosts, getAllTags, getPostsTopPage } from '@/lib/notionAPI'
 import { SinglePost } from './components/Post/SinglePost'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
@@ -12,16 +12,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const getStaticProps: GetStaticProps = async () => {
   const Posts = await getPostsTopPage(4);
+  const allTags = await getAllTags();
 
   return {
     props: {
       Posts,
+      allTags,
     },
     revalidate: 60,
   }
 }
 
-export default function Home({ Posts }) {
+export default function Home({ Posts, allTags }) {
   return (
     <div className='container h-full w-full mx-auto'>
       <Head>
@@ -53,7 +55,9 @@ export default function Home({ Posts }) {
         >
           ...もっとみる
         </Link>
-        <Tag />
+        <Tag
+          tags={allTags}
+        />
       </main>
     </div>
   )
